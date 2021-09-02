54222f2a58cd4b670b4571ccd8f383f5dd58288d
ValeriaGallegoR <valeriagallego20@gmail.com>
2021-09-02 11:28:10 -0500

    Se modifico las clases Televisor y Nevera

--- src/Models/Nevera.java
+++ src/Models/Nevera.java
@@ -1,4 +1,10 @@
 package Models;
 
 public class Nevera {
+    public void capacidadNevera (double precio){
+        int litros = 0;
+        for (int i; litros > 120; i++){
+            precio += 0.5;
+        }
+    }
 }
--- src/Models/Televisor.java
+++ src/Models/Televisor.java
@@ -1,4 +1,22 @@
 package Models;
 
 public class Televisor {
+    private int pulgadas;
+    private boolean tdt;
+    private double precio;
+
+    public Televisor(int pulgadas, boolean tdt){
+        this.pulgadas = pulgadas;
+        this.tdt = tdt;
+    }
+    public boolean tamañoTV(int pulgadas){
+        if (this.pulgadas > 40){
+            this.precio += 0.30;
+        } else {
+            return precio;
+        }
+        if(tdt == true ){
+            this.precio += 250.000;
+        }
+    }
 }
